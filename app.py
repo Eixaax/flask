3552c06e70a2da6c6469955d3259ce392938c1e6
Eixaax <133875664+Eixaax@users.noreply.github.com>
2025-01-05 11:45:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+import eventlet
+eventlet.monkey_patch()  # Ensure this is the first line of code
+
 from flask import Flask
 from flask_socketio import SocketIO, emit
 from pymongo import MongoClient
-
 
 app = Flask(__name__)
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 client = MongoClient("mongodb+srv://isa:admin@cluster0.v0xnx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Replace with your MongoDB URI
-db = client['test'] 
-collection = db['Device'] 
+db = client['test']
+collection = db['Device']
 
 @app.route('/')
 def index():
@@ -18,13 +20,14 @@ def index():
 @socketio.on('data')
 def handle_device_data(data):
     device_name = data.get('deviceName')
-
     device = collection.find_one({"deviceName": device_name})
 
     if device:
         emit('response', {'success': True, 'message': 'Device Connected!'})
+        print('connected!')
     else:
         emit('response', {'success': False, 'message': 'Device Not Found'})
+        print('not found!')
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
